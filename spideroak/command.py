89f2f2f1fc76c0b3fda2703709ce1baf7d43cf13
Samuel J. Woodward <sam@woodward.fyi>
2025-08-27 11:42:52 -0400

    cleanup

--- spideroak/command.py
+++ spideroak/command.py
@@ -7,7 +7,9 @@ from spideroak import cli_path, tail, _system
 
 def run(*args, redirect_stdout=False, capture_output=True, **kwargs):
     if not redirect_stdout:
-        return subprocess.run([cli_path, *args], **kwargs)
+        return subprocess.run(
+            [cli_path, *args], capture_output=capture_output, **kwargs
+        )
     delete = False if _system == 'Windows' else True
     try:
         with tempfile.NamedTemporaryFile(
@@ -21,7 +23,9 @@ def run(*args, redirect_stdout=False, capture_output=True, **kwargs):
                 )
                 tail_thread.start()
                 proc = subprocess.run(
-                    [cli_path, *args, f'--redirect={tmp_file.name}'], **kwargs
+                    [cli_path, *args, f'--redirect={tmp_file.name}'],
+                    capture_output=capture_output,
+                    **kwargs,
                 )
             except (KeyboardInterrupt, Exception) as e:
                 tail_thread.stop()
